dc9654fdee5846b9aecd1ba3fc937d5d12e2fd2f
SamuelLowry <samueldlowry@gmail.com>
2020-10-01 17:06:51 -0400

    code and comments done

--- scripts/03-blog.R
+++ scripts/03-blog.R
@@ -35,6 +35,7 @@ grade_20_df <- read_csv("https://raw.githubusercontent.com/fivethirtyeight/data/
   drop_na(x538_grade)
 
 #grade plots
+#formatted to actually be readable on website
 grade_16_plot <- grade_16_df %>% 
   ggplot(aes(x = x538_grade)) +
   geom_bar(fill = 'darkblue') +
@@ -47,6 +48,8 @@ grade_16_plot <- grade_16_df %>%
         axis.title.x = element_text(size = 16),
         axis.title.y = element_text(size = 16))
 
+#grade plots
+#formatted to actually be readable on website
 grade_20_plot <- grade_20_df %>% 
   ggplot(aes(x = x538_grade)) +
   geom_bar(fill = 'darkblue') +
@@ -79,7 +82,7 @@ inc_fund_df <- econ_df %>%
   filter(incumbent_party == TRUE,
          incumbent == TRUE)
 
-#plots showing model based off of second quarter gdp growth for both the challenger and the incumbent
+#plots showing model based off of second quarter gdp growth for the challenger
 chal_plot <- chal_fund_df %>% 
   ggplot(aes(x = gdp_growth_qt, y = pv)) +
   geom_point(alpha = 0) +
@@ -95,6 +98,7 @@ chal_plot <- chal_fund_df %>%
         axis.title.x = element_text(size = 16),
         axis.title.y = element_text(size = 16))
 
+#plots showing model based off of second quarter gdp growth for the incumbent
 inc_plot <- inc_fund_df %>% 
   ggplot(aes(x = gdp_growth_qt, y = pv)) +
   geom_point(alpha = 0) +
@@ -116,6 +120,7 @@ plot_grid(inc_plot, chal_plot)
 #saved for blog
 ggsave("figures/fund_plot.png", height = 6, width = 12)
 
+#plot showing september 2020 polls
 recent_poll_plot <- polls_2020_df %>% 
   filter(stage == "general",
          candidate_name %in% c("Joseph R. Biden Jr.", "Donald Trump")) %>%
@@ -144,6 +149,7 @@ recent_poll_plot <- polls_2020_df %>%
         axis.title.x = element_text(size = 16),
         axis.title.y = element_text(size = 16))
 
+#plot showing a polls back a year before the election
 old_poll_plot <- polls_2020_df %>% 
   filter(stage == "general",
          candidate_name %in% c("Joseph R. Biden Jr.", "Donald Trump")) %>%
@@ -195,11 +201,13 @@ poll_pred_df <- polls_2020_df %>%
   group_by(candidate_name) %>%
   summarize(sum = sum(weighted_pct * 2))
 
+#dataframe needed with q2 growth from 2020
 corona_df <- econ_df %>% 
   filter(year == 2020,
          quarter == 2) %>% 
   select(gdp_growth_qt)
 
+#model for trump with confidence intervals, fundamentals
 inc_fund_model <- lm(formula = pv ~ gdp_growth_qt, data = inc_fund_df)
 trump_fund_pred <- as.data.frame(predict(inc_fund_model, corona_df,
                  interval = "prediction", 
@@ -207,7 +215,7 @@ trump_fund_pred <- as.data.frame(predict(inc_fund_model, corona_df,
   mutate(se = (upr - fit) / 1.96) %>%
   mutate(type = "Trump Fundamentals")
   
-
+#model for biden with confidence intervals, fundamentals
 chal_fund_model <- lm(formula = pv ~ gdp_growth_qt, data = chal_fund_df)
 biden_fund_pred <- as.data.frame(predict(chal_fund_model,
                  corona_df,
@@ -215,7 +223,8 @@ biden_fund_pred <- as.data.frame(predict(chal_fund_model,
                  level=0.95)) %>%
   mutate(se = (upr - fit) / 1.96) %>% 
   mutate(type = "Biden Fundamentals")
-  
+
+#created weights for trump's polls
 weighted_trump_df <- polls_2020_df %>% 
   filter(stage == "general",
          candidate_name %in% c("Joseph R. Biden Jr.", "Donald Trump")) %>%
@@ -231,16 +240,21 @@ weighted_trump_df <- polls_2020_df %>%
   filter(days_out <= 59) %>% 
   filter(candidate_name == "Donald Trump")
 
+#used survey package to assign weights
 trump_poll_pred <- svydesign(id = ~1, weights = ~weight, data = weighted_trump_df)
 
+#and used it again to get the estimates
 trump_poll_pred <- svymean(~pct, trump_poll_pred)
 
+#turned it into a tibble
 trump_poll_pred <- tibble(fit = 41.999,
                           se = 0.1607) %>% 
+  #link for equations https://www.healthknowledge.org.uk/e-learning/statistical-methods/practitioners/standard-error-confidence-intervals
   mutate(upr = (1.96 * se) + fit,
          lwr = fit - (1.96 * se)) %>%
   mutate(type = "Trump Poll")
 
+#created weights for biden's polls
 weighted_biden_df <- polls_2020_df %>% 
   filter(stage == "general",
          candidate_name %in% c("Joseph R. Biden Jr.", "Donald Trump")) %>%
@@ -256,32 +270,42 @@ weighted_biden_df <- polls_2020_df %>%
   filter(days_out <= 59) %>% 
   filter(candidate_name == "Joseph R. Biden Jr.")
   
+#used survey package to assign weights
 biden_poll_pred <- svydesign(id = ~1, weights = ~weight, data = weighted_biden_df)
 
+#and used it again to get the estimates
 biden_poll_pred <- svymean(~pct, biden_poll_pred)
 
+#turned it into a tibble
 biden_poll_pred <- tibble(fit = 50.717,
-                          se = 0.1878) %>% 
+                          se = 0.1878) %>%
+  #link for equations https://www.healthknowledge.org.uk/e-learning/statistical-methods/practitioners/standard-error-confidence-intervals
   mutate(upr = (1.96 * se) + fit,
          lwr = fit - (1.96 * se)) %>%
   mutate(type = "Biden Poll")
 
+#http://mathbench.org.au/statistical-tests/testing-differences-with-the-t-test/6-combining-sds-for-fun-and-profit/ math for standard error combination
+#.87 and .13 per poll weighting found here https://fivethirtyeight.com/features/how-fivethirtyeights-2020-presidential-forecast-works-and-whats-different-because-of-covid-19/
 biden_comb_pred <- tibble(fit = ((.87 * biden_poll_pred$fit[1]) + (.13 * biden_fund_pred$fit[1])),
                           se = sqrt((.87 * biden_poll_pred$se[1])^2 + (.13 * biden_fund_pred$se[1])^2)) %>% 
   mutate(upr = (1.96 * se) + fit,
          lwr = fit - (1.96 * se)) %>%
   mutate(type = "Biden Ensemble")
 
+#http://mathbench.org.au/statistical-tests/testing-differences-with-the-t-test/6-combining-sds-for-fun-and-profit/ math for standard error combination
 trump_comb_pred <- tibble(fit = ((.87 * trump_poll_pred$fit[1]) + (.13 * trump_fund_pred$fit[1])),
                           se = sqrt((.87 * trump_poll_pred$se[1])^2 + (.13 * trump_fund_pred$se[1])^2)) %>% 
   mutate(upr = (1.96 * se) + fit,
          lwr = fit - (1.96 * se)) %>%
   mutate(type = "Trump Ensemble")
 
+#combine all for one estimate table
 estimate_df <- rbind(trump_fund_pred, biden_fund_pred, trump_poll_pred, biden_poll_pred, trump_comb_pred, biden_comb_pred)
 
-estimate_df <- estimate_df[, c(5, 1, 2, 3, 4)]
+#reordered columns using this link for reference http://www.sthda.com/english/wiki/reordering-data-frame-columns-in-r
+estimate_df <- estimate_df[, c(5, 1, 2, 3, 4)] 
 
+#made estimate gt table using this link for reference https://gt.rstudio.com/articles/intro-creating-gt-tables.html
 estimate_table <- estimate_df %>% 
   gt() %>% 
   tab_header(
@@ -293,5 +317,7 @@ estimate_table <- estimate_df %>%
     lwr = "Lower Bound",
     upr = "Upper Bound",
     se = "Standard Error") %>% 
-  tab_source_note(source_note = md("Showing 95% confidence intervals"))
-
+  tab_source_note(source_note = md("Showing 95% confidence intervals")) %>% 
+  #formatted numbers using this link https://gt.rstudio.com/reference/fmt_number.html
+  fmt_number(decimals = 2,
+             columns = 2:5)
